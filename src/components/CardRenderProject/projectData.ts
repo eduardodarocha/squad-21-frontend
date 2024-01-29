bc5d552f675daa2bffdd19ce91e4093d81e486ed
gleisonfernandes <gleison.fernandesb@gmail.com>
2024-01-29 01:34:40 -0300

    :sparkles: feat: add card render project component

---
+++ src/components/CardRenderProject/projectData.ts
@@ -0,0 +1,34 @@
+import { ProjectProps } from "../../pages/portfolio/types";
+
+export const projectsData: ProjectProps[] = [
+    {
+      "avatar": "assets/images/avatar40.png",
+      "image": "assets/images/landingpage.png",
+      "author": "Camila Soares",
+      "date": "06/2023",
+      "tags": ["Dev", "UX"
+    ]
+    },
+    {
+      "avatar": "assets/images/avatar40.png",
+      "image": "assets/images/landingpage.png",
+      "author": "Camila Soares",
+      "date": "11/2023",
+      "tags": ["Dev", "Web"]
+    },
+    {
+      "avatar": "assets/images/avatar40.png",
+      "image": "../../../assets/images/landingpage.png",
+      "author": "Camila Soares",
+      "date": "01/2024",
+      "tags": ["UX", "Web"]
+    },
+    {
+      "avatar": "assets/images/avatar40.png",
+      "image": "../../../assets/images/landingpage.png",
+      "author": "Camila Soares",
+      "date": "01/2024",
+      "tags": ["UX", "Dev", "Web"]
+    }
+  ]
+  
