0a7aa2b7a16780b6a73cba7e1bc34eb411262376
gleisonfernandes <gleison.fernandesb@gmail.com>
2024-01-29 01:29:11 -0300

    :sparkles: feat: add cardUser component

---
+++ src/components/CardUser/index.tsx
@@ -0,0 +1,32 @@
+import { Box, Button, Card, CardContent, CardMedia, Typography } from "@mui/material"
+
+const CardUser = () => {
+    return (
+
+        <Box sx={{ display: 'flex', justifyContent: "center" }}>
+            <Card sx={{ display: 'flex', gap: '42px', width: '364px', height: "122px", boxShadow: 'none' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ width: 122, borderRadius: 50 }}
+                    image="../assets/images/avatar122.png"
+                    alt="Avatar ou imagem do perfil"
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+                    <CardContent sx={{ padding: 0 }}>
+                        <Typography variant="h5">
+                            Camila Soares
+                        </Typography>
+                        <Typography variant="subtitle1" color="text.secondary">
+                            Brasil
+                        </Typography>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Button type="submit" variant="contained" size="medium" color="secondary">ADICIONAR PROJETO</Button>
+                    </Box>
+                </Box>
+            </Card>
+        </Box>
+
+    )
+}
+export default CardUser;
